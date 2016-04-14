f807e7a0b54c6bd7268a6ff278fc3a78cc344d36
Jacob Jackson <jacob.t.jackson@gmail.com>
2016-04-14 00:50:29 -0500

    finish checkpoint 23 event scripting

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,12 +1,23 @@
-var animatePoints = function() {
- 
-     var points = document.getElementsByClassName('point');
- 
-     var revealPoint = function(i = 0, i < 3, i++) {
-         points[i].style.opacity = 1;
-         points[i].style.transform = "scaleX(1) translateY(0)";
-         points[i].style.msTransform = "scaleX(1) translateY(0)";
-         points[i].style.WebkitTransform = "scaleX(1) translateY(0)";
+var pointsArray = document.getElementsByClassName('point');
+
+var animatePoints = function(points) {
+     var revealPoint = function(index) {
+         points[index].style.opacity = 1;
+         points[index].style.transform = "scaleX(1) translateY(0)";
+         points[index].style.msTransform = "scaleX(1) translateY(0)";
+         points[index].style.WebkitTransform = "scaleX(1) translateY(0)";
      };
+    for (var i = 0; i < points.length; i++){
      revealPoint(i);
+    };
+};
+
+window.onload = function() {
+    var sellingPoints = document.getElementsByClassName('selling-points')[0];
+    var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+    window.addEventListener('scroll', function(event) {
+         if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
+             animatePoints(pointsArray);
+         }
+     });
 };
