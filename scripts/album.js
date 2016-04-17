84b273f016454d3d0f435989d13717f6603814cd
Jacob Jackson <jacob.t.jackson@gmail.com>
2016-04-17 18:05:14 -0500

    rewrite findParentByClassName()

--- scripts/album.js
+++ scripts/album.js
@@ -114,11 +114,16 @@ var currentlyPlayingSong = null;
 
  window.onload = function() {
      setCurrentAlbum(albumPicasso);
-     songListContainer.addEventListener('mouseover', function(event){
-         var songItem = getSongItem(event.target);
-         if (songItem.getAttribute('data-song-number') !== currentlyPlayingSong) {
-            songItem.innerHTML = playButtonTemplate;
+
+     songListContainer.addEventListener('mouseover', function(event) {
+        if (event.target.parentElement.className === 'album-view-song-item') {
+            var songItem = getSongItem(event.target);
+
+            if (songItem.getAttribute('data-song-number') !== currentlyPlayingSong) {
+                songItem.innerHTML = playButtonTemplate;
             }
+        }
+     });
 
      for (var i = 0; i < songRows.length; i++) {
          songRows[i].addEventListener('mouseleave', function(event) {
