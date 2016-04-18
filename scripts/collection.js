14d09eb189aaefb6163a36d612a49b03c6aa4c3a
Jacob Jackson <jacob.t.jackson@gmail.com>
2016-04-18 02:39:11 -0500

    complete checkpoint 31 jquery play/pause

--- scripts/collection.js
+++ scripts/collection.js
@@ -1,30 +1,27 @@
-
-var buildCollecitonTemplate = function(){
-  var template =
+var buildCollectionItemTemplate = function() {
+    var template = 
     '<div class="collection-album-container column fourth">'
-+ '  <img src="assets/images/album_covers/01.png"/>'
-+ '  <div class="collection-album-info caption">'
-+ '    <p>'
-+ '      <a class="album-name" href="/album.html"> The Colors </a>'
-+ '      <br/>'
-+ '      <a href="/album.html"> Pablo Picasso </a>'
-+ '      <br/>'
-+ '      X songs'
-+ '      <br/>'
-+ '    </p>'
-+ '  </div>'
-+ '</div>'
-
+   + '  <img src="assets/images/album_covers/01.png"/>'
+   + '  <div class="collection-album-info caption">'
+   + '    <p>'
+   + '      <a class="album-name" href="/bloc-jams/album.html"> The Colors </a>'
+   + '      <br/>'
+   + '      <a href="/bloc-jams/album.html"> Pablo Picasso </a>'
+   + '      <br/>'
+   + '      X songs'
+   + '      <br/>'
+   + '    </p>'
+   + '  </div>'
+   + '</div>'
+   ;
     return $(template);
 };
-;
 
-$(window).load(function(){
-  var $collectionContainer = $('.album-covers');
-  $collectionContainer.empty();
-
-  for(var i=0; i<12;i++){
-    var $newThumbnail = buildCollecitonTemplate();
-    $collectionContainer.append($newThumbnail);
-  }
+$(window).load(function() {
+    var $collectionContainer = $('.album-covers');
+    $collectionContainer.empty();
+    for (var i = 0; i < 12; i++) {
+        var $newThumbnail = buildCollectionItemTemplate();
+        $collectionContainer.append($newThumbnail);
+    }
 });
